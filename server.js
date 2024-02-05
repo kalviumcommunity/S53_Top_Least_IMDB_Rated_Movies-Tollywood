8b455219f2c83d0dec4427f38f71d34f6321ee94
jahnavreddy <jahnavredy12@gmail.com>
2024-02-05 11:22:11 +0530

    packagenodemon

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-app.listen(3000);
+const PORT= process.env.PORT 
+app.listen(PORT , ()=>{
+  console.log(`http://localhost:${PORT}`);
+});
 
 app.get("/ping", (req, res) => res.send("pong"));
+app.get("/", (req, res) => res.send("Hello!!This is Home Page"));
+
