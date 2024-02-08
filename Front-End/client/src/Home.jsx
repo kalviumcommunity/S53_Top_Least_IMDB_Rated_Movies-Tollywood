5965260be1b45364683d11d850b329d74a02b1d5
jahnavreddy <jahnavredy12@gmail.com>
2024-02-08 10:14:44 +0530

    home

---
+++ Front-End/client/src/Home.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function Home() {
+  return(
+    <>
+    <p className="welcome">Welcome to ASAP Project!!!</p>
+    <button>Click here to Login/Signup</button>
+    </>
+  )
+}
+
+export default Home
