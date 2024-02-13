1e2deb895a37583a49baa0e2ecdb7ecceeb09866
jahnavreddy <jahnavredy12@gmail.com>
2024-02-13 10:55:39 +0530

    done

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,14 +2,14 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+app.use(express.json())
 const connectDB = require("./db");
 connectDB();
 const PORT = process.env.PORT;
 const router = require("./routes");
 const cors = require("cors");
 app.use(cors());
-app.use("/movies", router);
-
+app.use(router);
 app.get("/ping", (req, res) => res.send("pong"));
 app.get("/", (req, res) => res.send("Hello!!This is Home Page"));
 
