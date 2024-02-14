1b35aafc97fab64178ef0bab6e787d233918ffce
jahnavreddy <jahnavredy12@gmail.com>
2024-02-14 10:30:17 +0530

    main

--- Front-End/client/src/Components/Home.jsx
+++ Front-End/client/src/Components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
   useEffect(() => {
     async function fetchData() {
-      const url = "http://localhost:3000/movies/movies";
+      const url = "https://s53-top-least-imdb-rated-movies-tollywood.onrender.com/movies/movies";
       axios.get(url).then((res) => {
         console.log(res);
         setData(res.data);
