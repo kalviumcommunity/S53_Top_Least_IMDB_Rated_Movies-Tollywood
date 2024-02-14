2a7f691fe87cdcce67d99bd08c1b81375cea32ba
jahnavreddy <jahnavredy12@gmail.com>
2024-02-14 10:33:22 +0530

    updatedLink

--- Back-End/routes.js
+++ Back-End/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-router.use("/movies",router)
+app.use("/movies",router)
 const dataModel = require("./Schema");
 
 
