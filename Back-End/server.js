bf70e3a6c2e198b5d5f7f7bf59968810218af272
jahnavreddy <jahnavredy12@gmail.com>
2024-02-15 16:36:43 +0530

    joiImplemented

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-
+const joi = require("joi")
 const app = express();
 app.use(express.json())
 const connectDB = require("./db");
@@ -11,9 +11,30 @@ const cors = require("cors");
 app.use(cors());
 app.use("/movies", router);
 
+
 app.get("/ping", (req, res) => res.send("pong"));
 app.get("/", (req, res) => res.send("Hello!!This is Home Page"));
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
+
+const schema = joi.object({
+  Hero : joi.string().min(3).max(12).required(),
+  Title : joi.string().required().min(3).max(18),
+  Ratings : joi.number().required(),
+  Director : joi.string().min(3).required(),
+  Image : joi.string().required()
+
+})
+
+app.post("/movies/create",(req,res)=>{
+
+  const {error , value} = schema.validate(req.body)
+  if(error){
+    console.log("Invalid Input");
+  }else{
+    console.log("Success");
+  }
+
+})
